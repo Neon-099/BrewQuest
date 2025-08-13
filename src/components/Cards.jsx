3547f555a527f3f0211b74fff7f417214e37cd78
Neon-099 <Emmannatividad099@gmail.com>
2025-08-13 12:03:10 +0800

    added: words limit for each coffee item

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,6 +1,18 @@
+import { useState } from "react";
+
 
 
 const Cards = ( {title, img, description, link, price} ) => {
+    const [isLong, setIsLong ] = useState(false);
+    const limit = 88;
+
+    //WORD BASED
+    const word = description.split("");
+
+    //CHECK IF WE NEED TO SEE MORE
+    const isLongText = word.length > limit;
+    const displayedText = isLong ? description : word.slice(0, limit).join('') + (isLongText ? '...' : '');
+
     return (
         <div className="my-10 h-96 flex flex-col mb-30"> {/*NEED TO SET A MAX HEIGHT FOR EVERY CARD (including...)*/}
             <img
@@ -8,7 +20,15 @@ const Cards = ( {title, img, description, link, price} ) => {
                 src={img}
                 alt="" />
             <p className="py-3 font-semibold">{title}</p>
-            <p className="text-[#96734F] text-sm py-4">{description}</p>
+            <p className="text-[#96734F] text-sm py-4">{displayedText}</p>
+            {isLong && (
+                <button 
+                    onClick={() => setIsLong(!isLong)}
+                     className="text-blue-500 hover:underline mt-1"
+                    >
+                    {isLong ? "See Less" : "See More"}
+                </button>
+            )}
             <p className="text-md text-[#96734F]">{price}</p>
         </div>
     )
